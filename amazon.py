b864410c4702288f8ebd5f6381ff264a50fe0655
Jason R. Coombs <jaraco@jaraco.com>
2020-02-01 21:25:38 -0500

    Expand work on importing

--- amazon.py
+++ amazon.py
@@ -7,14 +7,18 @@ from com.infinitekind.moneydance import model
 
 __metaclass__ = type
 
+# suppress linter warning for Py3
+if False:
+    long = int
 
-def create_transaction(account_name):
-    root = moneydance.getCurrentAccount()
+
+def create_transaction(account):
     book = moneydance.getCurrentAccountBook()
     txn = model.ParentTxn(book)
-    txn.date = int(datetime.date.today().strftime('%Y%m%d'))
-    account = root.getAccountByName(account_name)
+    txn.date = txn.taxDate = int(datetime.date.today().strftime('%Y%m%d'))
     txn.account = account
     spl = model.SplitTxn(txn)
-    spl.setAmount(1)
+    spl.setAmount(long(1))
+    txn.addSplit(spl)
     txn.syncItem()
+    return txn, spl
--- demo.py
+++ demo.py
@@ -2,10 +2,11 @@ import itertools
 
 from com.infinitekind.moneydance import model
 
+import demo
 from __main__ import moneydance
 
 
-def print_transactions(limit=10):
+def _print_transactions(limit=10):
     # get the default environment variables, set by Moneydance
     print("The Moneydance app controller: %s" % (moneydance))
     print("The current data set: %s" % (moneydance.getCurrentAccountBook()))
@@ -24,16 +25,15 @@ def print_transactions(limit=10):
         )
 
 
-def print_transactions_for_account(account_name):
-    root = moneydance.getCurrentAccount()
+def acct(name="Amazon Purchases"):
+    return moneydance.getCurrentAccount().getAccountByName(name)
+
+
+def get_transactions(account):
     book = moneydance.getCurrentAccountBook()
+    return book.getTransactionSet().getTransactionsForAccount(account)
 
-    account = root.getAccountByName(account_name)
-    assert account
 
-    txns = book.getTransactionSet().getTransactionsForAccount(account)
-    for txn in txns:
+def print_transactions(account, limit=10):
+    for txn in itertools.islice(get_transactions(account), limit):
         print(txn)
-
-
-print_transactions_for_account('Amazon Purchases')
