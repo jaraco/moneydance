13e15c82c548f25f0c471bb07f7f5d040017385b
Jason R. Coombs <jaraco@jaraco.com>
2022-09-16 21:05:13 -0400

    Implement export and import.

--- sidebar.py
+++ sidebar.py
@@ -1,4 +1,10 @@
+"""
+Export and import the accounts from the sidebar.
+"""
+
 from __future__ import print_function
+
+import os
 
 from com.moneydance.apps.md.view.gui.sidebar import FullSideBarItemList
 from com.moneydance.apps.md.view.gui.sidebar.nodes import SideBarNodeFactory, SideBarNodeType
@@ -6,20 +12,45 @@ from com.moneydance.apps.md.view.gui.sidebar.nodes import SideBarNodeFactory, Si
 global moneydance
 
 
-def export_node(node):
-    if node.getChildType() != SideBarNodeType.ACCOUNT:
-        return
-    acctId = node.getAccountId()
-    acct = book.getAccountByNum(acctId)
-    active = not acct.getAccountisInactive()
-    print(node, acctId, acct, active)
+default_name = os.path.join(os.path.dirname(__file__), 'sidebar accounts.txt')
 
-def export_nodes():
+
+def _is_account(node):
+    return node.getChildType() == SideBarNodeType.ACCOUNT
+
+
+def _load_nodes():
     book = moneydance.getCurrentAccountBook()
     mdGUI = moneydance.getUI()
     dtm = SideBarNodeFactory.getBarModelFromSettings(book, mdGUI)
-    nodes = SideBarNodeFactory.getNodesFromTree(dtm)
-    consume(map(export_node, nodes))
+    return SideBarNodeFactory.getNodesFromTree(dtm)
 
-def save_nodes(nodes):
+
+def export(dest=default_name):
+    account_nodes = filter(_is_account, _load_nodes())
+    with open(dest, 'w') as outf:
+        outf.writelines(str(node) + '\n' for node in account_nodes)
+
+
+def import_(src=default_name, dry_run=True):
+    current = _load_nodes()
+    with open(src) as inf:
+        expected = [line.strip() for line in inf]
+    remove_nodes = [
+        node for node in current if _is_account(node) and str(node) not in expected]
+    if not remove_nodes:
+        print("Nothing to remove")
+        return
+    action = "Simulating removing" if dry_run else "Removing"
+    print(action, remove_nodes, "from sidebar")
+    if dry_run:
+        return
+    for node in remove_nodes:
+        current.remove(node)
+    _save_nodes(current)
+
+
+def _save_nodes(nodes):
+    book = moneydance.getCurrentAccountBook()
+    mdGUI = moneydance.getUI()
     SideBarNodeFactory.saveNodesToSettings(book, mdGUI, nodes)
